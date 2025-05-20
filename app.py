a9d7a6e049f4e343f7b8bd3245efeb531494f947
aaryashah1010@gmail.com <your-email@example.com>
2025-05-20 18:27:21 +0530

    commi

--- app.py
+++ app.py
@@ -83,5 +83,8 @@ def index():
 
     return render_template("index.html", result_text=result_text, parsed_json=parsed_json)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets the PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
